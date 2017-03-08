e206d56da29cd54ff76c41d308b748b73b4ca2ef
wpiusa <wpiusa@gmail.com>
2017-03-08 14:31:03 -0800

    Add files via upload

---
+++ 5_3/js/app.js
@@ -0,0 +1,14 @@
+//Employee name, age, salary, city, state and pin code 
+
+var person={
+	name:"Tom Lee",
+	age:52,
+	salary:100000,
+	address:{
+      city:"LA",
+      state:"CA",
+      zipcode:"11111"
+	}
+}
+
+console.log(person);
